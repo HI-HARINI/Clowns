66be9e58a3298ffb3ff45a868e53bf08f1f6bb08
HI-HARINI <77035916+HI-HARINI@users.noreply.github.com>
2021-08-24 19:54:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -9,14 +9,14 @@ canvas.center()
 video=createCapture(VIDEO)
 video.size(300,300)
 video.hide()
-postNet=ml5.postNet(video,modelLoaded)
+postNet=ml5.poseNet(video,modelLoaded)
 postNet.on('pose',gotPoses)
 }
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        nosex=results[0].pose.nose.x;
-        nosey=results[0].pose.nose.y;
+        nosex=results[0].pose.nose.x-15;
+        nosey=results[0].pose.nose.y-15;
     }
 }
 function modelLoaded(){
@@ -24,7 +24,7 @@ function modelLoaded(){
 }
 function draw(){
 image(video,0,0,300,300);
-image(clownnose,nosex,nosey,30,30)
+image(clownnose,nosex,nosey,20,20)
 }
 function takesnapshot(){
     save("filter.png")
